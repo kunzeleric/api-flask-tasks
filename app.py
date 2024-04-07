bc3b065813d39a73469ef2b8dbf7cf3527cf7a85
Eric Kunzel <kunzel.eric@gmail.com>
2024-04-07 10:26:20 -0300

    feat: created other CRUD routes for tasks

--- app.py
+++ app.py
@@ -6,15 +6,55 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
+# create a new task
 @app.route("/tasks", methods=["POST"])
 def create_task():
     global task_id_control
     data = request.get_json()
+
     new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ""))
     task_id_control += 1
     tasks.append(new_task)
+
     return jsonify({ "message": 'New Task created successfully!'})
 
+# get all tasks
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {"tasks": task_list, "total_tasks": len(task_list)}
+
+    return jsonify(output)
+
+# get task by id
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task_by_id(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Task not found!"}), 404
+
+# update task by id
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def update_task(id):
+    for t in tasks:
+        if t.id == id:
+            data = request.get_json()
+            t.title = data['title']
+            t.description = data.get("description", "")
+            t.completed = data.get("completed", False)
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Task not found!"}), 404
+
+#delete task by id
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+    for t in tasks:
+        if t.id == id:
+            tasks.remove(t)
+            return jsonify({"message": "Task deleted successfully!"}), 200
+    return jsonify({"message": "Task not found!"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
